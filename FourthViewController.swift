78062b9d1a1fda53370a529b8d0a38bda9234c29
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-08-15 00:01:29 -0500

    Added a new table view for the Lifeguarding class information.

--- FourthViewController.swift
+++ FourthViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class FourthVIewController: UIViewController {
+class FourthVIewController: UIViewController, UITableViewDelegate   {
     
+    @IBOutlet var options: UITableView!
+    
+
+    var cellContent = ["Prerequisites", "Lifeguarding Class", "Lifeguarding Manuel", "Completion", "Other"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellContent.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = cellContent[indexPath.row]
+        return cell
+        
+    }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
